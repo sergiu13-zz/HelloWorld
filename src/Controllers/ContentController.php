379583fc3b5035f0c7f5735b5415e0e98c6ee1aa
Sergiu Stan <sis@project2commerce.com>
2017-04-05 11:58:30 +0300

    Searching for packages

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -5,11 +5,33 @@ namespace HelloWorld\Controllers;
 
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Modules\Plugin\Libs\Contracts\LibraryCallContract;
+use Plenty\Plugin\Http\Request;
 
+/**
+ * Class ContentController
+ * @package HelloWorld\Controllers
+ */
 class ContentController extends Controller
 {
-    public function sayHello(Twig $twig):string
+    /**
+     * @param Twig $twig
+     * @param LibraryCallContract $libCall
+     * @param Request $request
+     * @return string
+     */
+    public function sayHello(
+        Twig $twig,
+        LibraryCallContract $libCall,
+        Request $request
+    )
     {
-        return $twig->render('HelloWorld::content.hello');
+
+        $packagistResult =
+            $libCall->call(
+                'HelloWorld::guzzle_connector',
+                ['packagist_query' => $request->get('search')]
+            );
+        return $twig->render('HelloWorld::content.hello', $packagistResult);
     }
 }
